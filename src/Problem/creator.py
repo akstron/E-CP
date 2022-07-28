edc57449e85e79331c0596c0a3275b457b41f220
akstron <62210712+akstron@users.noreply.github.com>
2022-07-28 23:51:56 +0530

    code_file creator added

--- src/Problem/creator.py
+++ src/Problem/creator.py
@@ -1,12 +1,45 @@
+from pathlib import Path
 import click
 import os
 from Scrapers.cf_scraper import get_problem as get_cf_problem
 
-#TODO: Handle directory name collision
+#TODO: Add default dest = '.'
 @click.command()
-@click.argument('dest', type=str)
 @click.argument('url', type=str)
+@click.argument('dest', type=str, default='.')
 def problem(dest, url):
     problem = get_cf_problem(url)
-    dir = f'{dest}\\{problem.name}'
-    # os.mkdir(dir)
+
+    # Create directory
+    dir = Path(dest, problem.name)
+
+    # If directory is not already created
+    if(not dir.exists()):
+        os.mkdir(dir)
+
+    # Create test files
+    tests = problem.tests
+    index = 1
+    for test in tests:
+        input_test_file = Path(dir, f'input_{index}.txt')
+        with open(input_test_file, 'w') as file:
+            file.write(test.input)
+        
+        output_test_file = Path(dir, f'output_{index}.txt')
+        with open(output_test_file, 'w') as file:
+            file.write(test.output)
+
+        index += 1
+
+    # Create code file -> Default to cpp
+    # TODO: Change default
+
+    code_file = Path(dir, f'code.cpp')
+    ''' 
+        'w' with overwrite the file, even if we don't write anything 
+        https://stackoverflow.com/questions/16208206/confused-by-python-file-mode-w#:~:text=w%2B-,Opens%20a%20file%20for%20both%20writing%20and%20reading.,file%20for%20reading%20and%20writing.
+    '''
+    with open(code_file, 'a') as file:
+        pass
+
+    
