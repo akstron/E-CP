d5d68ca12929fcad5fc44918621f2ecb79722b8a
akstron <62210712+akstron@users.noreply.github.com>
2022-08-02 19:21:49 +0530

    custom input runner added

--- src/Cli/runner.py
+++ src/Cli/runner.py
@@ -1,3 +1,4 @@
+from email.policy import default
 import click
 from ..Runner.error import UnsupportedLanguage
 from ..Config.config import get_lang
@@ -5,14 +6,15 @@ from ..Runner.mapper import runner_map
 
 @click.command()
 @click.argument('dest', type=str, default='.')
-def run_code(dest):
+@click.option('-c', is_flag = True, default = False)
+def run(dest, c):
     try:
         lang = get_lang()
         if(lang not in runner_map):
             raise UnsupportedLanguage(lang)
 
         runner = runner_map[lang]
-        runner(dest)
+        runner(dest, c)
     except UnsupportedLanguage as e:
         click.echo(e)
     except Exception as e:
--- src/Runner/runner.py
+++ src/Runner/runner.py
@@ -7,19 +7,34 @@ import subprocess
 import click
 from .error import CodeError
 from .comparator import validate_output
+from abc import ABC, abstractmethod
 
-# dest is the directory path
-def run_cpp_code(dest):
-    try:
-        code_file = Path(dest, 'code.cpp')
+class Runner(ABC):
+    def __init__(self, dest, code_file) -> None:
+        self.dest = dest
+        self.code_file = code_file
 
-        # Create executable file
-        compile_result = subprocess.run(['g++', code_file, '-o', 'a'], capture_output=True, text=True)
+    @abstractmethod
+    def run_on_test_files(self):
+        pass
 
-        if compile_result.returncode != 0:
-            raise CodeError(compile_result.stderr, 'compile error',compile_result.returncode)
+    @abstractmethod
+    def run_on_custom(self):
+        pass
 
-        current_dir = os.listdir(dest)
+class CppRunner(Runner):
+    def __init__(self, dest, code_file) -> None:
+        super().__init__(dest, code_file)
+        self.exec_file = self.__compile_code()
+
+    def __compile_code(self):
+        return subprocess.run(['g++', self.code_file, '-o', 'a'], capture_output=True, text=True)
+
+    def __execute_code(self, exec_file, stdin = None):
+        return subprocess.run([exec_file], capture_output=True, text=True, stdin=stdin)
+
+    def run_on_test_files(self):
+        current_dir = os.listdir(self.dest)
 
         for file_name in current_dir:
             if len(file_name) >= 5:
@@ -29,7 +44,7 @@ def run_cpp_code(dest):
                     input_file_path = Path(input_file_name)
                     with open(input_file_path, 'r') as content:   
                         # Run executable file with sample input files
-                        exec_result = subprocess.run(['./a'], capture_output=True, text=True, stdin=content)
+                        exec_result = self.__execute_code('./a', content)
 
                         if exec_result.returncode != 0:
                             raise CodeError(exec_result.stderr, 'runtime error', exec_result.returncode)
@@ -39,42 +54,73 @@ def run_cpp_code(dest):
                         # Write output obtained from to output_file
                         with open(output_file_path, 'w') as output_file:
                             output_file.write(exec_result.stdout.strip()) 
-        
-        validate_output(dest)
+
+    def run_on_custom(self):
+        exec_result = self.__execute_code(Path(self.dest, 'a'))
+        click.echo(exec_result.stdout)
+
+class PythonRunner(Runner):
+    def __init__(self, dest, code_file) -> None:
+        super().__init__(dest, code_file)
+    
+    def __execute_code(self, stdin = subprocess.PIPE):
+        return subprocess.run(['python', 'code.py'], capture_output=True, text=True, stdin=stdin)
+
+    def run_on_test_files(self):
+        current_dir = os.listdir(self.dest)
+
+        for file_name in current_dir:
+            if len(file_name) >= 5:
+                if file_name[:5] == 'input' and file_name[-4:] == '.txt':
+                    input_file_name = file_name
+                    
+                    input_file_path = Path(input_file_name)
+                    with open(input_file_path, 'r') as content:   
+                        # Run executable file with sample input files
+                        exec_result = self.__execute_code(content)
+
+                        if exec_result.returncode != 0:
+                            raise CodeError(exec_result.stderr, 'runtime error', exec_result.returncode)
+
+                        output_file_path = Path(f'output{file_name[5:]}')
+
+                        # Write output obtained from to output_file
+                        with open(output_file_path, 'w') as output_file:
+                            output_file.write(exec_result.stdout.strip()) 
+    
+    def run_on_custom(self):
+        exec_result = self.__execute_code()
+        click.echo(exec_result.stdout)
+
+# dest is the directory path
+def run_cpp_code(dest, custom):
+    try:
+        code_file = Path(dest, 'code.cpp')
+        runner = CppRunner(dest, code_file)
+
+        if custom:
+            runner.run_on_custom()
+        else :
+            runner.run_on_test_files()
+            validate_output(dest)
         
     except CodeError as e:
-        click.echo(f'Error type: {e.error_type}')
-        click.echo(f'Return code: {e.return_code}')
         click.echo(e)
 
     except Exception as e:
-        print(e)
-        print('Something went wrong')
+        click.echo(e)
 
-def run_python_code(dest):
+def run_python_code(dest, custom):
     try:
-        current_dir = os.listdir(dest)
+        code_file = Path(dest, 'code.py')
+        runner = PythonRunner(dest, code_file)
 
-        for file_name in current_dir:
-            if len(file_name) >= 5:
-                if file_name[:5] == 'input' and file_name[-4:] == '.txt':
-                    input_file_name = file_name
-                    
-                    input_file_path = Path(input_file_name)
-                    with open(input_file_path, 'r') as content:   
-                        # Run executable file with sample input files
-                        exec_result = subprocess.run(['python', 'code.py'], capture_output=True, text=True, stdin=content)
+        if custom:
+            runner.run_on_custom()
+        else :
+            runner.run_on_test_files()
+            validate_output(dest)
 
-                        if exec_result.returncode != 0:
-                            raise CodeError(exec_result.stderr, 'runtime error', exec_result.returncode)
-
-                        output_file_path = Path(f'output{file_name[5:]}')
-
-                        # Write output obtained from to output_file
-                        with open(output_file_path, 'w') as output_file:
-                            output_file.write(exec_result.stdout.strip()) 
-
-        validate_output(dest)
     except CodeError as e:
         click.echo(e)
     except Exception as e:
